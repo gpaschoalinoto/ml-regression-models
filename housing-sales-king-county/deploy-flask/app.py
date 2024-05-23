288a1ca2a63b7c9470f5a402c8b7c25fc2bc9c56
Guilherme Paschoalinoto <62107649+gpaschoalinoto@users.noreply.github.com>
2024-05-23 14:58:44 -0300

    Deploy Flask application Heroku

--- housing-sales-king-county/deploy-flask/app.py
+++ housing-sales-king-county/deploy-flask/app.py
@@ -1,59 +1,60 @@
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
 
-# Creating app
 app = Flask(__name__)
 
-# Home page
-@app.route("/")
+# Loading models
+model_lgbm = pickle.load(open('../../housing-sales-king-county/deploy-flask/models/model_lgbm_tuned.pkl', 'rb'))
+model_rf = pickle.load(open('../../housing-sales-king-county/deploy-flask/models/model_rfr_tuned.pkl', 'rb'))
+
+@app.route('/')
 def index():
-    result = None
-    return render_template("index_house.html", result = result)
+    return render_template('index_house.html')
 
-# Prediction page
-@app.route("/predict", methods = ["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
-    values = request.form.getlist('new_house')
-    df_to_predict = pd.DataFrame({  'num_bed':[values[0]],
-                                    'num_bath':[values[1]],
-                                    'size_house':[values[2]],
-                                    'size_lot':[values[3]],
-                                    'num_floors':[values[4]],
-                                    'is_waterfront':[values[5]],
-                                    'condition':[values[6]],
-                                    'size_basement':[values[7]],
-                                    'year_built':[values[8]],
-                                    'renovation_date':[values[9]],
-                                    'zip':[values[10]],
-                                    'latitude':[values[11]],
-                                    'longitude':[values[12]],
-                                    'avg_size_neighbor_houses':[values[13]],
-                                    'avg_size_neighbor_lot':[values[14]]
-                                })
     
-    df_to_predict = df_to_predict.astype(dtype= {'num_bed':'int64',
-                                    'num_bath':'float64',
-                                    'size_house':'int64',
-                                    'size_lot':'int64',
-                                    'num_floors':'float64',
-                                    'is_waterfront':'int64',
-                                    'condition':'int64',
-                                    'size_basement':'int64',
-                                    'year_built':'int64',
-                                    'renovation_date':'int64',
-                                    'zip':'int64',
-                                    'latitude':'float64',
-                                    'longitude':'float64',
-                                    'avg_size_neighbor_houses':'int64',
-                                    'avg_size_neighbor_lot':'int64'})
-    
-    model = pickle.load(open('../../housing-sales-king-county/deploy-flask/models/model_lgbm_tuned.pkl', 'rb'))
-    print('\ndf: ', df_to_predict)
-    result = model.predict(df_to_predict)
-    result = "%.2f" % result
-    return render_template('index_house.html', result = result)
+    def format_currency(value):
+    # Format the value as dollar with dot as thousands separator and comma for decimals
+        return "${:,.2f}".format(value).replace(',', ';').replace('.', ',').replace(';', '.')
 
-# Execute application
-if __name__ == "__main__":
-    app.run(host = 'localhost', port = 3000, debug = True)
+    try:
+        values = request.form.getlist('new_house')
+        model_choice = request.form.get('model')
+
+        df_to_predict = pd.DataFrame({
+            'num_bed': [int(values[0])],
+            'num_bath': [float(values[1])],
+            'size_house': [int(values[2])],
+            'size_lot': [int(values[3])],
+            'num_floors': [float(values[4])],
+            'is_waterfront': [int(values[5])],
+            'condition': [int(values[6])],
+            'size_basement': [int(values[7])],
+            'year_built': [int(values[8])],
+            'renovation_date': [int(values[9])],
+            'zip': [int(values[10])],
+            'latitude': [float(values[11])],
+            'longitude': [float(values[12])],
+            'avg_size_neighbor_houses': [int(values[13])],
+            'avg_size_neighbor_lot': [int(values[14])]
+        })
+
+        if model_choice == 'compare_both_models':
+            prediction_rf = model_rf.predict(df_to_predict)
+            prediction_lgbm = model_lgbm.predict(df_to_predict)
+            return jsonify({'prediction_rf': format_currency(prediction_rf[0]),
+                            'prediction_lgbm': format_currency(prediction_lgbm[0])
+                            })
+        elif model_choice == 'random_forest':
+            prediction = model_rf.predict(df_to_predict)
+        else:
+            prediction = model_lgbm.predict(df_to_predict)
+
+        return jsonify({'prediction': format_currency(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000, debug=True)
